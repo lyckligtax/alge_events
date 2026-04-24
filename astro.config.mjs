6d4ff2b1ca584dfbb9c8ecb97dcefb3dfed4e58b
tsubklewe <tobias@lyckligtax.com>
2026-04-25 00:01:03 +0200

    mobile friendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  server: {
+      host: '0.0.0.0',
+  }
 });
